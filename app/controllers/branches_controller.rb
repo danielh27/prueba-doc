9bb8ed5b0b0ce2e753dcd7e25ca4be6d25ba325c
Daniel Herrera <daniel97hh@gmail.com>
2022-03-01 18:13:42 -0500

    create new branch form

--- app/controllers/branches_controller.rb
+++ app/controllers/branches_controller.rb
@@ -2,4 +2,25 @@ class BranchesController < ApplicationController
   def index
     @branches = Branch.where(user: current_user)
   end
+
+  def new
+    @branch = Branch.new
+  end
+
+  def create
+    @branch = Branch.new(branch_params)
+    @branch.user = current_user
+
+    if @branch.save
+      redirect_to branches_path
+    else
+      render :new
+    end
+  end
+
+  private
+
+  def branch_params
+    params.require(:branch).permit(:name, :street_name, :neighborhood_name, :indoor_number, :outdoor_number, :postal_code, :city, :country)
+  end
 end
--- app/models/branch.rb
+++ app/models/branch.rb
@@ -5,8 +5,8 @@ class Branch < ApplicationRecord
   validates :name, presence: true
   validates :street_name, presence: true
   validates :neighborhood_name, presence: true
-  validates :outdoor_number, presence: true, numericality: { only_integer: true }
-  validates :indoor_number, presence: true, numericality: { only_integer: true }
+  validates :outdoor_number, numericality: { only_integer: true }
+  validates :indoor_number, numericality: { only_integer: true }
   validates :postal_code, presence: true, format: { with: /\A\d{5}\z/ }
   validates :country, presence: true
   validates :city, presence: true
